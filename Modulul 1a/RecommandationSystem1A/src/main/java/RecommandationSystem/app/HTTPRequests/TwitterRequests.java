1370d1b1e7f1dae1407357a53e6267d03eaddc50
Tudor Carare <tudor.carare@gmail.com>
2015-05-04 22:35:18 +0300

    change pom to spring

--- Modulul 1a/RecommandationSystem1A/src/main/java/RecommandationSystem/app/HTTPRequests/TwitterRequests.java
+++ Modulul 1a/RecommandationSystem1A/src/main/java/RecommandationSystem/app/HTTPRequests/TwitterRequests.java
@@ -7,4 +7,11 @@ public class TwitterRequests
 {
     private static String twitterApiKey = "NSvMPuP62LUadR9anpbeg4k7i";
     private static String twitterSecretKey = "bbRyqzWpgi2teT1m3bb7TalM0TweVuWuXlRptmyTjm2shLXZw0";
+
+    private static String API_URL = "";
+
+    public void testRequest()
+    {
+
+    }
 }
