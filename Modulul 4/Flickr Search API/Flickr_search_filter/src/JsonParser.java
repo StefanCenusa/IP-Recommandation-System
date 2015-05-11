79ba699b10e839063594af2f3bffd94dda761fbe
Veronica Cojocaru <veronica.cojocaru11@gmail.com>
2015-05-12 00:41:05 +0300

    more updates

---
+++ Modulul 4/Flickr Search API/Flickr_search_filter/src/JsonParser.java
@@ -0,0 +1,56 @@
+
+import org.json.*;
+import sun.misc.IOUtils;
+import sun.nio.ch.IOUtil;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.ParseException;
+
+/**
+ * Created by Alin on 5/11/2015.
+ */
+public class JsonParser {
+
+    private static JSONObject jObj;
+
+    public static void getJsonFromPath(String path)throws IOException
+    {
+            String jsonText = readFile(path, Charset.defaultCharset());
+            jObj = new JSONObject(jsonText);
+
+    }
+    public static String getValueByKey(String key)
+    {
+        String returnedString;
+        if(jObj!=null)
+        {
+            returnedString = jObj.getString(key);
+        }
+        else
+        {
+            System.out.print("apeleaza setJsonPath");
+            returnedString = null;
+        }
+
+        return returnedString;
+    }
+
+    static String readFile(String path, Charset encoding)
+            throws IOException
+    {
+        byte[] encoded = Files.readAllBytes(Paths.get(path));
+        return new String(encoded, encoding);
+    }
+    //public String[] getArrayByKey(String)
+    //JSONObject obj = new JSONObject(" .... ");
+    //String pageName = obj.getJSONObject("pageInfo").getString("pageName");
+
+//    JSONArray arr = obj.getJSONArray("posts");
+     //  String post_id = arr.getJSONObject(i).getString("post_id");
+
+}
